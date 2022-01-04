93242ebb0e7e0097b9477a9c3577ff6767df113b
Vee Hole <vee.hole77@hotmail.com>
2022-01-04 23:51:39 +0800

    work in progress

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2022_01_04_074948) do
 
   # These are extensions that must be enabled in order to support this database
@@ -64,8 +63,6 @@ ActiveRecord::Schema.define(version: 2022_01_04_074948) do
     t.string "address"
     t.bigint "user_id", null: false
     t.string "image"
-    t.date "date_from"
-    t.date "date_to"
     t.float "latitude"
     t.float "longitude"
     t.index ["user_id"], name: "index_places_on_user_id"
