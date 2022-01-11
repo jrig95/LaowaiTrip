f78666a26bc83f074a232cb2f2fdd027fc188a56
Sean <seanalexclark@gmail.com>
2022-01-11 14:24:42 +0800

    added more seeds to the file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,35 +5,129 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# puts "Cleaning database..."
-# Place.destroy_all
-# puts "Creating places..."
-# require "open-uri"
-# first_user = User.first
+puts "Cleaning database..."
+Place.destroy_all
+puts "Creating places..."
+require "open-uri"
+first_user = User.first
 
-# first_place_image = URI.open('https://t-cf.bstatic.com/xdata/images/hotel/max1280x900/327419823.jpg?k=482a587ee9cd4aca228e48c7e6d38173fafd2981e93dd9367532b2a93d0c8f6b&o=&hp=1')
-# first_place = Place.new(user: first_user, city_name: "Shanghai", place_name: "Shanghai W on the Bund", description: "A beautiful hotel on the north bund ", price_by_night: 159, view: "bund view", available: true,max_guest: 4,number_bathrooms: 4, address:"66 Lvshun Road, Hongkou, Shanghai, China", number_rooms: 4, amenities: "washer and dryer, towel rack, hot tub")
-# first_place.photos.attach(io: first_place_image, filename: 'shanghai_w.jpeg', content_type: 'image/jpeg')
-# first_place.save!
 
-# second_place_image_one = URI.open('https://images.getaroom-cdn.com/image/upload/s--Y55dO4d_--/c_limit,e_improve,fl_lossy.immutable_cache,h_940,q_auto:good,w_940/v1620722391/abfa10e5894f5f001b68f34605e400fc5c666a6d?atc=e7cd1cfa')
-# second_place_image_two = URI.open('https://cdn.kiwicollection.com/media/property/PR008349/xl/008349-01-exterior-shaghai-skyline.jpg?cb=1435069697')
-# second_place = Place.new(user: first_user, city_name: "Shanghai", place_name: "The Peace Hotel", description: "A beautiful hotel next to the bund", price_by_night: 160, view: "bund view", available: true,max_guest: 6, number_bathrooms: 2, address:"20 East Nanjing Road, Shanghai, China",number_rooms: 4, amenities: "washer and dryer, towel rack, hot tub")
+# Shanghai Places
 
-# second_place.photos.attach(io: second_place_image_one, filename: 'shanghai_peace.jpeg', content_type: 'image/jpeg')
-# second_place.photos.attach(io: second_place_image_two, filename: 'shanghai_peace.jpeg', content_type: 'image/jpeg')
+# 1. W on the Bund/ Trip.com
+first_place_image_one = URI.open('https://t-cf.bstatic.com/xdata/images/hotel/max1280x900/327419823.jpg?k=482a587ee9cd4aca228e48c7e6d38173fafd2981e93dd9367532b2a93d0c8f6b&o=&hp=1')
+first_place_image_two = URI.open('https://ak-d.tripcdn.com/images/0204g1200097ae0x0D5A0_Z_1080_808_R5_D.jpg')
+first_place_image_three = URI.open('https://ak-d.tripcdn.com/images/200p0k000000c6v4p95A5_Z_1080_808_R5_D.jpg')
+first_place = Place.new(user: first_user, city_name: "Shanghai", place_name: "Shanghai W on the Bund", description: "A beautiful hotel on the north bund ", price_by_night: 281, view: "bund view", available: true,max_guest: 4,number_bathrooms: 1, address:"No. 66 Lvshun Road, Shanghai, China", number_rooms: 4, amenities: "washer and dryer, towel rack, hot tub", link: "https://us.trip.com/hotels/shanghai-hotel-detail-7270168/w-shanghai-the-bund/?cityId=2&checkIn=2022-01-10&checkOut=2022-01-11&adult=1&children=0&crn=1&ages=&travelpurpose=0&curr=USD&link=button&hoteluniquekey=H4sIAAAAAAAAAONaw8jFK8Fo8B8GGIWYOBilpjFyPD3fd4fJgMORAQS-zHbwBDPMFjgE8ExilOKE8QQ9HlaJrDOb4-DExrHqH6MEywzGGfd2M29kBCt4UOiwg5HpBONMpgVMM4HCu5igqg4xsXIcZJZgOsXEcImJ4RYTwyMmhldMDJ-YGH7BlDQxM3QxM0xihiicxcywiJlBiBXsLCkF46Q0M3MDS2PjZEtDExODlERLUwNzSxMzi1TDJMMUQxMFbo1Tq973sRmJnWIUNjQzMTCzsLS0MDQ3MNHLTjHKN7BilmJ082AMYjM0cLOwdI2S4WJ29osUBDua4YO9FHNosItiePTG_W_qJB2SWFPzdEODMy5yFDB2MbIJMHowRjCuYmQAABjpz2o9AQAA", thirdparty: "trip.com" )
+first_place.photos.attach(io: first_place_image_one, filename: 'shanghai_w.jpeg', content_type: 'image/jpeg')
+first_place.photos.attach(io: first_place_image_two, filename: 'shanghai_w.jpeg', content_type: 'image/jpeg')
+first_place.photos.attach(io: first_place_image_three, filename: 'shanghai_w.jpeg', content_type: 'image/jpeg')
+first_place.save!
 
-# second_place.save!
+# 2. Peace Hotel/ Trip.com
+second_place_image_one = URI.open('https://images.getaroom-cdn.com/image/upload/s--Y55dO4d_--/c_limit,e_improve,fl_lossy.immutable_cache,h_940,q_auto:good,w_940/v1620722391/abfa10e5894f5f001b68f34605e400fc5c666a6d?atc=e7cd1cfa')
+second_place_image_two = URI.open('https://cdn.kiwicollection.com/media/property/PR008349/xl/008349-01-exterior-shaghai-skyline.jpg?cb=1435069697')
+second_place_image_three = URI.open('https://ak-d.tripcdn.com/images/20080m000000dh47mF827_Z_1080_808_R5_D.jpg')
+second_place = Place.new(user: first_user, city_name: "Shanghai", place_name: "The Peace Hotel", description: "A beautiful hotel next to the bund", price_by_night: 160, view: "bund view", available: true,max_guest: 6, number_bathrooms: 2, address:"20 East Nanjing Road, Shanghai, China",number_rooms: 4, amenities: "washer and dryer, towel rack, hot tub", link: "https://us.trip.com/hotels/shanghai-hotel-detail-375539/fairmont-peace-hotel/?cityId=2&checkIn=2022-01-10&checkOut=2022-01-11&adult=1&children=0&crn=1&ages=&travelpurpose=0&curr=USD&link=title&hoteluniquekey=H4sIAAAAAAAAAON6yMjFK8Fo8B8GGIWYOBilTjNyHNrU185s8drYkQEEbkx38AQzHsx2COApBDMdQh0mMdpwgtkmcx0EwYwtMx2UFBRYNGEqDGEMi1a2IFaObVwSLFEMTmwcfU8ZJVhmMH7-KraREaxEocBhByPTCcb3jAuYPk2dxrWLCaroEBMrx1l5CZZTTAyXmBhuMTE8YoKY84qJ4RMTwy-YuiZmhi5mhknMENWzmCGKFjEzCLGC_SKlYGqcaGlgZJJmYmqSZGJolmZpZmiaZpZibJSSnGJmZGyhwK3Ru-19H5uR2ClGYUMzEwMzC0tLC0NzAxO97BSjfAMrZilGNw_GIDYTE0dXI5coGS5mZ79IiL8ZPthLMYcGuygyV3CraNRJOiSxpubphgZnXOQoYOxiZBNg9GCMYFzFyAAAkpvXwXIBAAA", thirdparty: "trip.com")
 
-# third_place_image = URI.open('https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/95126675.jpg?k=6c74e8b295ad1527ebefea7b56f96a2ac3e6ec49344d089a462b3abca62131e8&o=&hp=1')
-# third_place = Place.new(user: first_user, city_name: "Shanghai", place_name: "JW Marriot", description: "A beautiful hotel in People's Square", price_by_night: 160, view: "bund view", available: true,max_guest: 6, number_bathrooms: 2, address:"399 Nanjing West Road, Shanghai, China",number_rooms: 4, amenities: "washer and dryer, towel rack, hot tub")
-# third_place.photos.attach(io: third_place_image, filename: 'shanghai_peace.jpeg', content_type: 'image/jpeg')
-# third_place.save!
+second_place.photos.attach(io: second_place_image_one, filename: 'shanghai_peace.jpeg', content_type: 'image/jpeg')
+second_place.photos.attach(io: second_place_image_two, filename: 'shanghai_peace.jpeg', content_type: 'image/jpeg')
+second_place.photos.attach(io: second_place_image_three, filename: 'shanghai_peace.jpeg', content_type: 'image/jpeg')
 
-# puts "Done"
+second_place.save!
 
-require_relative 'scrapers'
+# 3. JW Marriot/ Trip.com
+third_place_image_one = URI.open('https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/95126675.jpg?k=6c74e8b295ad1527ebefea7b56f96a2ac3e6ec49344d089a462b3abca62131e8&o=&hp=1')
+third_place_image_two = URI.open('https://ak-d.tripcdn.com/images/20090a00000056siw14F1_Z_1080_808_R5_D.jpg')
+third_place_image_three = URI.open('https://ak-d.tripcdn.com/images/20080m000000dh47mF827_Z_1080_808_R5_D.jpg')
+third_place = Place.new(user: first_user, city_name: "Shanghai", place_name: "JW Marriot", description: "A beautiful hotel in People's Square", price_by_night: 160, view: "bund view", available: true,max_guest: 6, number_bathrooms: 2, address:"399 Nanjing West Road, Shanghai, China",number_rooms: 4, amenities: "washer and dryer, towel rack, hot tub", link: "https://us.trip.com/hotels/shanghai-hotel-detail-375216/jw-marriott-hotel-shanghai-at-tomorrow-square/?cityId=2&checkIn=2022-01-10&checkOut=2022-01-11&adult=1&children=0&crn=1&ages=&travelpurpose=0&curr=USD&link=title&hoteluniquekey=H4sIAAAAAAAAAONaysjFK8Fo8B8GGIWYOBil-hk5fh_6PoPJkQEEMrodPMGME_0OATyTGCU5YTxBW67riwuWdjs4sXJcrZFgmcG44bPYRkawdEOiww5GphOMXYwLmB4dlt3FBFFyCEjPFJRgOcXEcImJ4RYTwyMmhldMDJ-YGH5BVTQxM3QxM0xihqibxcywiJlBiBXsHikF02QD82QL8-S0JPM0k6QUcwsLw0RTAwsTA2OLtBQDw1QFbo3Nm9_3sRmJnWIUNjQzMTCzsLS0MDQ3MNHLTjHKN7BilmJ082AMYnMyMDc2M4-S4WJ29osUBLuY4YO9FHNosIsicwW3ikadpEMSa2qebmhwxkWOAqA32AQYPRgjGFcxMgAAhc0S0zYBAAA", thirdparty: "trip.com")
+third_place.photos.attach(io: third_place_image_one, filename: 'shanghai_jw.jpeg', content_type: 'image/jpeg')
+third_place.photos.attach(io: third_place_image_two, filename: 'shanghai_jw.jpeg', content_type: 'image/jpeg')
+third_place.photos.attach(io: third_place_image_three, filename: 'shanghai_jw.jpeg', content_type: 'image/jpeg')
+third_place.save!
 
-p Scrape
-scrape = Scrape.new
-p scrape.trip_scraper
+# 4. SH Airbnb
+fourth_place_image_one = URI.open('https://a0.muscache.com/im/pictures/df7d1f00-ae3a-4706-8e4e-884dad8bdb41.jpg?im_w=1200')
+fourth_place_image_two = URI.open('https://a0.muscache.com/im/pictures/5ad9723e-ded5-4093-b930-74679fa37145.jpg?im_w=720')
+fourth_place_image_three = URI.open('https://a0.muscache.com/im/pictures/miso/Hosting-19821414/original/66b89f5f-0c07-4551-8e5b-5048265998b3.jpeg?im_w=720')
+fourth_place = Place.new(user: first_user, city_name: "Shanghai", place_name: "Bund Japanese folk style", description: "Located on the 23rd floor of Hengsheng Peninsula International Center Building, No. 205, Wujie Road, Hongkou District, the Japanese-style tatami room with river view in the North Bund, all photos are authentic and taken on your mobile phone.", price_by_night: 590, view: "bund view", available: true,max_guest: 6, number_bathrooms: 2, address:"No. 205, Wujie Road, Hongkou District, Shanghai, China",number_rooms: 4, amenities: "washer and dryer, towel rack, hot tub", link: "https://www.airbnb.com/rooms/19821414?check_in=2022-01-11&check_out=2022-01-12&federated_search_id=353f85ab-6ae7-4fd0-b225-2e4bb071b1f0&source_impression_id=p3_1641800411_L7bd%2BKYqKprH4Km8", thirdparty: "airbnb")
+fourth_place.photos.attach(io: fourth_place_image_one, filename: 'shanghai_airbbnb.jpeg', content_type: 'image/jpeg')
+fourth_place.photos.attach(io: fourth_place_image_two, filename: 'shanghai_airbnb.jpeg', content_type: 'image/jpeg')
+fourth_place.photos.attach(io: fourth_place_image_three, filename: 'shanghai_airbnb.jpeg', content_type: 'image/jpeg')
+fourth_place.save!
+
+# 5. SH Ritz Carlton
+fifth_place_image_one = URI.open('https://himg1.qunarzz.com/imgs/202103/06/C.C9-FU2C7Jw8os7JXA480s.jpg')
+fifth_place_image_two = URI.open('http://ugcimg.qunarzz.com/imgs/202108/23/Ol4mXmFbmhAL3KOWm1024.jpg')
+fifth_place_image_three = URI.open('http://ugcimg.qunarzz.com/imgs/202108/23/Ol4mXmFbmUrMWvNNm1024.jpg')
+fifth_place = Place.new(user: first_user, city_name: "Shanghai", place_name: "Pudong Ritz Carlton", description: "Ritz Carlton in Pudong", price_by_night: 590, view: "bund view", available: true,max_guest: 6, number_bathrooms: 2, address:"浦东新区陆家嘴世纪大道8号上海国金中心",number_rooms: 4, amenities: "washer and dryer, towel rack, hot tub", link: "https://hotel.qunar.com/cn/shanghai_city/dt-5666/?fromDate=2022-01-15&toDate=2022-01-16&highQuality=true&bellaId=9JEBCjVZy_Y7F-dbyGnOt", thirdparty: "qunar")
+fifth_place.photos.attach(io: fifth_place_image_one, filename: 'shanghai_ritz.jpeg', content_type: 'image/jpeg')
+fifth_place.photos.attach(io: fifth_place_image_two, filename: 'shanghai_ritz.jpeg', content_type: 'image/jpeg')
+fifth_place.photos.attach(io: fifth_place_image_three, filename: 'shanghai_ritz.jpeg', content_type: 'image/jpeg')
+fifth_place.save!
+
+# 6. SH Hostelworld
+sixth_place_image_one = URI.open('https://a.hwstatic.com/image/upload/f_auto,q_auto,w_1900,h_823,c_limit,e_sharpen,e_improve,e_vibrance:60/v1/propertyimages/4/46215/65.jpg')
+sixth_place_image_two = URI.open('https://a.hwstatic.com/image/upload/f_auto,q_auto,w_1900,h_823,c_limit,e_sharpen,e_improve,e_vibrance:60/v1/propertyimages/4/46215/23.jpg')
+sixth_place_image_three = URI.open('https://a.hwstatic.com/image/upload/f_auto,q_auto,w_1900,h_823,c_limit,e_sharpen,e_improve,e_vibrance:60/v1/propertyimages/4/46215/36.jpg')
+sixth_place = Place.new(user: first_user, city_name: "Shanghai", place_name: "Shanghai Blue Mountain International Youth Hostel", description: "
+Blue Mountain Bund Youth Hostel is centrally located near East Nanjing Road metro station, 'just five minutes' walk from Nanjing Road and ten minutes away from the Bund'. Our guests say this means it's 'near enough everything to be easy but quiet at night'.", price_by_night: 87, view: "bund view", available: true,max_guest: 6, number_bathrooms: 2, address:"350 South Shanxi Road, Shanghai, China",number_rooms: 4, amenities: "washer and dryer, towel rack, hot tub", link: "https://www.hostelworld.com/pwa/hosteldetails.php/Shanghai-Blue-Mountain-International-Youth-Hostel/Shanghai/46215?from=2022-01-11&to=2022-01-14&guests=2", thirdparty: "hostelworld")
+sixth_place.photos.attach(io: sixth_place_image_one, filename: 'shanghai_ritz.jpeg', content_type: 'image/jpeg')
+sixth_place.photos.attach(io: sixth_place_image_two, filename: 'shanghai_ritz.jpeg', content_type: 'image/jpeg')
+sixth_place.photos.attach(io: sixth_place_image_three, filename: 'shanghai_ritz.jpeg', content_type: 'image/jpeg')
+sixth_place.save!
+
+
+# Sanya Places
+
+# 1. Sanya Atlantis
+first_sanya_place_image_one = URI.open('https://ak-d.tripcdn.com/images/200s1900000169opw2822_Z_1080_808_R5_D.jpg')
+first_sanya_place_image_two = URI.open('https://ak-d.tripcdn.com/images/0204q120008vlcih67860_Z_1080_808_R5_D.jpg')
+first_sanya_place_image_three = URI.open('https://ak-d.tripcdn.com/images/200c0x000000l9uguF3F6_Z_1080_808_R5_D.jpg')
+first_sanya_place = Place.new(user: first_user, city_name: "Sanya", place_name: "Atlantis Sanya", description: "The Atlantis Sanya provides a great place for travelers to relax after a busy day. The Atlantis Sanya is an ideal choice for travelers who want to take in the sights and sounds of Sanya.", price_by_night: 2500, view: "beach view", available: true,max_guest: 6, number_bathrooms: 2, address:"36 Haitang North Road, Haitangwan, Sanya, Hainan, China",number_rooms: 4, amenities: "washer and dryer, towel rack, hot tub", link: "https://us.trip.com/hotels/sanya-hotel-detail-14249344/atlantis-sanya/?cityId=43&checkIn=2022-01-11&checkOut=2022-01-12&adult=1&children=0&crn=1&ages=&travelpurpose=0&curr=USD&link=title&hoteluniquekey=H4sIAAAAAAAAAONay8jFK8Fo8B8GGIWYOBilpjNy3N2xvovZYAOzIwMITFno4AlmWCxxCOCZxCjFCeMJVousc394aqGDExvH8r1MEiwzGBtuP2XbyAhWoFDqsIOR6QTjQ6YFTI1A4V1MUFWHmFg5NmlKsJxiYrjExHCLieERE8MrJoZPTAy_YEqamBm6mBkmMUMUzmJmWMTMIMQKdpeUgoGJuVliqqWJsXGSgUmyibFFiqlpWpKRiWliskWqaZKFArfG3a4vfWxGYqcYhQ3NTAzMLCwtLQzNDUz0slOM8g2smKUY3TwYg9jMHR2d3dyiZLiYnf0iBcGOZvhgL8UcGuyieCbW-wR7raRDEmtqnm5ocMYljgLGLkY2AUYPxgjGVYwMAPO4G-c-AQAA", thirdparty: "trip.com")
+first_sanya_place.photos.attach(io: first_sanya_place_image_one, filename: 'sanya.jpeg', content_type: 'image/jpeg')
+first_sanya_place.photos.attach(io: first_sanya_place_image_two, filename: 'sanya.jpeg', content_type: 'image/jpeg')
+first_sanya_place.photos.attach(io: first_sanya_place_image_three, filename: 'sanya.jpeg', content_type: 'image/jpeg')
+first_sanya_place.save!
+
+# 2. Houhai Airbnb
+second_sanya_place_image_one = URI.open('https://z1.muscache.cn/im/pictures/miso/Hosting-40930487/original/f695bfaf-f7e0-4580-8e5d-1af1aca472cd.jpeg?im_w=1200')
+second_sanya_place_image_two = URI.open('https://z1.muscache.cn/im/pictures/miso/Hosting-40930487/original/91efed73-cb54-41e5-83b3-ee23f580436a.jpeg?im_w=1440')
+second_sanya_place_image_three = URI.open('https://z1.muscache.cn/im/pictures/27f47222-af9e-4f94-ad0f-b49a013eb7e1.jpg?im_w=1440')
+second_sanya_place = Place.new(user: first_user, city_name: "Sanya", place_name: "Houhai Village Hidden Sea View Queen Bed Room", description: "Our guesthouse is located on the national coast in the Begong Bay area, by the beach, you can stroll out of the house and share a sea surf spot with Centipede Island", price_by_night: 648, view: "beach view", available: true,max_guest: 6, number_bathrooms: 2, address:"Sanya, Hainan, China",number_rooms: 4, amenities: "washer and dryer, towel rack, hot tub", link: "https://www.airbnb.com/rooms/40930487?federated_search_id=efb77d56-b73b-499b-a36b-e16b9c876271&source_impression_id=p3_1641875239_ZQY69HDGZP9l1E4n", thirdparty: "airbnb")
+second_sanya_place.photos.attach(io: second_sanya_place_image_one, filename: 'sanya.jpeg', content_type: 'image/jpeg')
+second_sanya_place.photos.attach(io: second_sanya_place_image_two, filename: 'sanya.jpeg', content_type: 'image/jpeg')
+second_sanya_place.photos.attach(io: second_sanya_place_image_three, filename: 'sanya.jpeg', content_type: 'image/jpeg')
+second_sanya_place.save!
+
+# 3. qunar sanya
+third_sanya_place_image_one = URI.open('https://himg1.qunarzz.com/imgs/202106/01/C.awmrM-9aZ6iRDa7EZ480s.jpg')
+third_sanya_place_image_two = URI.open('http://ugcimg.qunarzz.com/imgs/202111/23/gvGt76vhuRYkGk3Ug1024.jpg')
+third_sanya_place_image_three = URI.open('http://ugcimg.qunarzz.com/imgs/202111/23/gvGt76vhuRYV4R4lg1024.jpg')
+third_sanya_place = Place.new(user: first_user, city_name: "Sanya", place_name: "Pheonix Sanya", description: "Located on Pheonix island", price_by_night: 648, view: "beach view", available: true,max_guest: 6, number_bathrooms: 2, address:"Sanya, Hainan, China",number_rooms: 4, amenities: "washer and dryer, towel rack, hot tub", link: "https://hotel.qunar.com/cn/sanya/dt-32545/?fromDate=2022-01-15&toDate=2022-01-16&highQuality=false&bellaId=RlPa11gwJgy05N8ZcEWN-", thirdparty: "qunar")
+third_sanya_place.photos.attach(io: third_sanya_place_image_one, filename: 'sanya.jpeg', content_type: 'image/jpeg')
+third_sanya_place.photos.attach(io: third_sanya_place_image_two, filename: 'sanya.jpeg', content_type: 'image/jpeg')
+third_sanya_place.photos.attach(io: third_sanya_place_image_three, filename: 'sanya.jpeg', content_type: 'image/jpeg')
+third_sanya_place.save!
+
+# 4. hostelworld sanya
+fourth_sanya_place_image_one = URI.open('https://a.hwstatic.com/image/upload/f_auto,q_auto,w_1900,h_823,c_limit,e_sharpen,e_improve,e_vibrance:60/v1/propertyimages/5/5666/1.jpg')
+fourth_sanya_place_image_two = URI.open('https://a.hwstatic.com/image/upload/f_auto,q_auto,w_1900,h_823,c_limit,e_sharpen,e_improve,e_vibrance:60/v1/propertyimages/5/5666/tkpo8owr2jujeocdjdzw')
+fourth_sanya_place_image_three = URI.open('https://a.hwstatic.com/image/upload/f_auto,q_auto,w_1900,h_823,c_limit,e_sharpen,e_improve,e_vibrance:60/v1/propertyimages/5/5666/wiq5tcvnmpkf1bnoniiw')
+fourth_sanya_place = Place.new(user: first_user, city_name: "Sanya", place_name: "Sanya Blue Sky International Youth Hostel", description: "Blue Sky Hostel is 150 meters from the great beach of Dadonghai Bay", price_by_night: 648, view: "beach view", available: true,max_guest: 6, number_bathrooms: 2, address:"Dadonghai District, Sanya, China",number_rooms: 4, amenities: "washer and dryer, towel rack, hot tub", link: "https://hotel.qunar.com/cn/sanya/dt-32545/?fromDate=2022-01-15&toDate=2022-01-16&highQuality=false&bellaId=RlPa11gwJgy05N8ZcEWN-", thirdparty: "hostelworld")
+fourth_sanya_place.photos.attach(io: fourth_sanya_place_image_one, filename: 'sanya.jpeg', content_type: 'image/jpeg')
+fourth_sanya_place.photos.attach(io: fourth_sanya_place_image_two, filename: 'sanya.jpeg', content_type: 'image/jpeg')
+fourth_sanya_place.photos.attach(io: fourth_sanya_place_image_three, filename: 'sanya.jpeg', content_type: 'image/jpeg')
+fourth_sanya_place.save!
+
+
+
+puts "Done"
+
+
+
+# require_relative 'scrapers'
+
+# p Scrape
+# scrape = Scrape.new
+# p scrape.trip_scraper
