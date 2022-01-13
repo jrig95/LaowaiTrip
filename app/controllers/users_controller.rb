f887bdad44e4902d4177dab02aab65c45561765d
Sean <seanalexclark@gmail.com>
2022-01-13 19:29:35 +0800

    trying to add profile pic

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+  def show
+    # @user = User.current_user
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
+  resources :users, only: %i[show]
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :places do
@@ -7,5 +9,6 @@ Rails.application.routes.draw do
     resources :wishlists, only: [:new, :create, :destroy]
   end
   get 'profile', to: 'pages#profile'
-  get 'users/:id/new', to: 'pages#profile', as: :edit_user
+
+  # get 'users/:id/new', to: 'pages#profile', as: :edit_user
 end
