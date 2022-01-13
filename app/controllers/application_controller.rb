7e1e1cb40d806e1ed387accdc74ebf5ba5d19ec2
Sean <seanalexclark@gmail.com>
2022-01-13 20:10:47 +0800

    can upload profile pic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,5 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys:[:name, :image])
-
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
-  def show
-    # @user = User.current_user
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to profile_path
+    else
+      puts "Error"
+      puts @user.errors.full_messages
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:photo)
   end
 end
